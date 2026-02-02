4744ec403ad27bdb7a729745c8856abdc3c5f881
Sandra Pereira <sandrajesuspintopereira@gmail.com>
2026-02-02 14:38:58 +0000

    array de objetos criado

---
+++ gestorInventario.js
@@ -0,0 +1,17 @@
+const inventario = [
+ { id: 1, nome: "Teclado Mecânico RGB", preco: 85, categoria: ["Informática", "Periféricos"],
+emStock: true },
+ { id: 2, nome: "Frigideira a Ar (Air Fryer)", preco: 110, categoria: ["Eletrodomésticos", "Cozinha"],
+emStock: true },
+ { id: 3, nome: "Cadeira de Escritório Ergonómica", preco: 180, categoria: ["Escritório",
+"Mobiliário"], emStock: false },
+ { id: 4, nome: "Ecrã Curvo 27' 144Hz", preco: 250, categoria: ["Informática", "Ecrãs"], emStock:
+true },
+ { id: 5, nome: "Máquina de Café de Cápsulas", preco: 75, categoria: ["Eletrodomésticos",
+"Cozinha"], emStock: true },
+ { id: 6, nome: "Caderno Inteligente A5", preco: 30, categoria: ["Escritório", "Papelaria"], emStock:
+true },
+ { id: 7, nome: "Trotinete Elétrica", preco: 450, categoria: ["Lazer", "Mobilidade"], emStock: false },
+ { id: 8, nome: "Rato Sem Fios Vertical", preco: 55, categoria: ["Informática", "Periféricos"],
+emStock: true }
+];
